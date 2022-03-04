968cee9b78ab27e939417f05bf4f8b6219e2c686
JustAlexander <aleksep@gmail.com>
2022-03-04 14:36:43 +0000

    fix: remove ga

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import { AppProps } from 'next/app';
 import '@/styles/global.css';
 import '@fontsource/inter';
 import 'react-modal-video/css/modal-video.min.css';
-import Script from 'next/script';
 
 import { setup } from 'twind';
 import twindConfig from '../twind.config';
@@ -12,19 +11,5 @@ if (typeof window !== `undefined`) {
 }
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`} />
-      <Script id="ga-analytics">
-        {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
-          `}
-      </Script>
-      <Component {...pageProps} />;
-    </>
-  );
+  return <Component {...pageProps} />;
 }
