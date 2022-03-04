dd6fd7165e16d21bff5857fbb5f806849feb2384
JustAlexander <aleksep@gmail.com>
2022-03-04 15:21:51 +0000

    fix: next 12

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,27 @@ import { AppProps } from 'next/app';
 import '@/styles/global.css';
 import '@fontsource/inter';
 import 'react-modal-video/css/modal-video.min.css';
+import Script from 'next/script';
 
 import withTwindApp from '@twind/next/app';
 import twindConfig from '../twind.config';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`} />
+      <Script id="ga-analytics">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+          `}
+      </Script>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default withTwindApp(twindConfig, MyApp);
