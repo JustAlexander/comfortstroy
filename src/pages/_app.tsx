303814ef4e31e4f8df421c8f86d8e698d8b7ac27
JustAlexander <aleksep@gmail.com>
2022-03-06 14:58:21 +0000

    fix: another ga script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,16 +10,14 @@ import twindConfig from '../twind.config';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`} />
-      <Script id="ga-analytics">
+      <Script id="google-analytics" strategy="afterInteractive">
         {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
-          `}
+            window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+            ga('create', '${process.env.NEXT_PUBLIC_GA_ID}', 'auto');
+            ga('send', 'pageview');
+        `}
       </Script>
+      <Script src="https://www.google-analytics.com/analytics.js" strategy="afterInteractive" />
       <Component {...pageProps} />;
     </>
   );
