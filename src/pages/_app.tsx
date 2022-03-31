5e6fdbf3642d1246ceee3a203603391d545d19f7
JustAlexander <aleksep@gmail.com>
2022-03-31 15:46:46 +0000

    fix: add react-gtm

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,27 +2,18 @@ import { AppProps } from 'next/app';
 import '@/styles/global.css';
 import '@fontsource/inter';
 import 'react-modal-video/css/modal-video.min.css';
-import Script from 'next/script';
 import withTwindApp from '@twind/next/app';
 import twindConfig from '../twind.config';
+import { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-TLKJPMD' });
+  }, []);
+
   return (
     <>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-TLKJPMD');`,
-        }}
-      />
-      <noscript
-        dangerouslySetInnerHTML={{
-          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TLKJPMD" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-        }}
-      />
       <Component {...pageProps} />;
     </>
   );
