b702f54af5d36111cbf84687d401cf5efeba47ed
JustAlexander <aleksep@gmail.com>
2022-04-09 08:10:27 +0000

    feat: add gtm

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,21 +2,18 @@ import { AppProps } from 'next/app';
 import '@/styles/global.css';
 import '@fontsource/inter';
 import 'react-modal-video/css/modal-video.min.css';
-import Script from 'next/script';
 import withTwindApp from '@twind/next/app';
 import twindConfig from '../twind.config';
+import { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-TLKJPMD' });
+  }, []);
+
   return (
     <>
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-            window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
-            ga('create', '${process.env.NEXT_PUBLIC_GA_ID}', 'auto');
-            ga('send', 'pageview');
-        `}
-      </Script>
-      <Script src="https://www.google-analytics.com/analytics.js" strategy="afterInteractive" />
       <Component {...pageProps} />;
     </>
   );
