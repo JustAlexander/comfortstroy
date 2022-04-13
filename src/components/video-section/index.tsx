e0b54f66e4975115edd56bb4fb0a86ee8c60f545
JustAlexander <aleksep@gmail.com>
2022-04-13 15:48:32 +0000

    feat: change video thumbnail

--- src/components/video-section/index.tsx
+++ src/components/video-section/index.tsx
@@ -4,6 +4,7 @@ import Play from '@/constants/svg/play.svg';
 import React, { useState } from 'react';
 
 import ModalVideo from 'react-modal-video';
+import Image from 'next/image';
 
 export default function VideoSection() {
   const [videoOpen, setVideoOpen] = useState(false);
@@ -18,7 +19,19 @@ export default function VideoSection() {
         <div className={tw(`flex flex-col max-w-4xl mx-auto pt-28`)}>
           <div className={tw(`w-full`)}>
             <div className={tw(`relative shadow-2xl mx-6 lg:mx-0`)}>
-              <Preferences width="100%" height="100%" />
+              {/* <Preferences width="100%" height="100%" /> */}
+              <Image src="/images/remont-kvartir-v-gelendzhike.jfif" width={600} height={400} layout="responsive" alt="Ремонт квартир в Геленджик под ключ" />
+              <button
+                onClick={handleClick}
+                type="button"
+                className={tw(
+                  `w-auto absolute top-1/2 left-1/2 flex items-center transform
+                  -translate-y-1/2 -translate-x-1/2 rounded-full font-medium group p-4`,
+                )}
+                aria-label="play video"
+              >
+                <Play className={tw(`w-24 h-24 lg:w-36 lg:h-36 fill-current text-gray-200 group-hover:text-yellow-600 flex-shrink-0`)} />
+              </button>
               <button
                 onClick={handleClick}
                 type="button"
